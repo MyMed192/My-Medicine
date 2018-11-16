ed9c216d0778bbedc222accff49ff8f1c3bfb85e
MyMed192 <ina.botnaru@ati.utm.md>
2018-11-16 13:50:49 -0700

    customize list of medicines and treatments

--- app/src/main/java/com/example/nichi/mymed/MedicineFragment.java
+++ app/src/main/java/com/example/nichi/mymed/MedicineFragment.java
@@ -1,6 +1,7 @@
 package com.example.nichi.mymed;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,9 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -18,10 +24,11 @@ import android.widget.Toast;
  */
 public class MedicineFragment extends Fragment {
 
-
-    public MedicineFragment() {
-        // Required empty public constructor
-    }
+    MedicineAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    Activity mActivity;
 
 
     @Override
@@ -30,15 +37,102 @@ public class MedicineFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_medicine, container, false);
 
 
-        String[] medicines = {"Nurofen", "Carbune", "Nolpaza", "Hartil", "Rivotril"};
-        ListView medicineListView = (ListView) view.findViewById(R.id.medicinesList);
-        ArrayAdapter medicineList = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, medicines);
 
-        medicineListView.setAdapter(medicineList);
+        // get the listview
+        expListView = (ExpandableListView) view.findViewById(R.id.lvExp);
 
-        // Inflate the layout for this fragment
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new MedicineAdapter(getActivity(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                //Toast.makeText(getActivity(),
+                //        listDataHeader.get(groupPosition) + " Expanded",
+                //        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                //Toast.makeText(getActivity(),
+                //        listDataHeader.get(groupPosition) + " Collapsed",
+                //        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                //Toast.makeText(
+                //        getActivity(),
+                //        listDataHeader.get(groupPosition)
+                //                + " : "
+                //                + listDataChild.get(
+                //                listDataHeader.get(groupPosition)).get(
+                //                childPosition), Toast.LENGTH_SHORT)
+                //        .show();
+                return false;
+            }
+        });
+
         return view;
     }
 
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Ibuprofen");
+        listDataHeader.add("Durex");
+        listDataHeader.add("Nurofen");
+
+        // Adding child data
+        List<String> pastila1 = new ArrayList<String>();
+        pastila1.add("23 pills");
+
+
+        List<String> pastila2 = new ArrayList<String>();
+        pastila2.add("2 ");
+
+
+        List<String> pastila3 = new ArrayList<String>();
+        pastila3.add("A full pack(30pills)");
+
+
+        listDataChild.put(listDataHeader.get(0), pastila1); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), pastila2);
+        listDataChild.put(listDataHeader.get(2), pastila3);
+    }
+
+
 }
+
--- app/src/main/java/com/example/nichi/mymed/TreatmentsFragment.java
+++ app/src/main/java/com/example/nichi/mymed/TreatmentsFragment.java
@@ -1,14 +1,20 @@
 package com.example.nichi.mymed;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -17,9 +23,11 @@ import android.widget.ListView;
 public class TreatmentsFragment extends Fragment {
 
 
-    public TreatmentsFragment() {
-        // Required empty public constructor
-    }
+    TreatmentAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    Activity mActivity;
 
 
     @Override
@@ -28,15 +36,99 @@ public class TreatmentsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_treatments, container, false);
 
 
-        String[] treatments = {"Flu", "Headaches", "Bellyache", "Arthrosis", "Osteochondrosis"};
-        ListView treatmentListView = (ListView) view.findViewById(R.id.treatmentsList);
-        ArrayAdapter treatmentList = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, treatments);
+        // get the listview
+        expListView = (ExpandableListView) view.findViewById(R.id.lvExp2);
 
-            treatmentListView.setAdapter(treatmentList);
+        // preparing list data
+        prepareListData();
 
-        // Inflate the layout for this fragment
+        listAdapter = new TreatmentAdapter(getActivity(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                //Toast.makeText(getActivity(),
+                //        listDataHeader.get(groupPosition) + " Expanded",
+                //        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                //Toast.makeText(getActivity(),
+                //        listDataHeader.get(groupPosition) + " Collapsed",
+                //        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                //Toast.makeText(
+                //        getActivity(),
+                //        listDataHeader.get(groupPosition)
+                //                + " : "
+                //                + listDataChild.get(
+                //                listDataHeader.get(groupPosition)).get(
+                //                childPosition), Toast.LENGTH_SHORT)
+                //        .show();
+                return false;
+            }
+        });
+
         return view;
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Raceala");
+        listDataHeader.add("Gripa");
+        listDataHeader.add("Siphilis");
+
+        // Adding child data
+        List<String> pastila1 = new ArrayList<String>();
+        pastila1.add("25 september 2018");
+
+
+        List<String> pastila2 = new ArrayList<String>();
+        pastila2.add("Mo zarazit Ina si stau de mai mult de o luna racit");
+
+
+        List<String> pastila3 = new ArrayList<String>();
+        pastila3.add("A full pack(30pills)");
+
+
+        listDataChild.put(listDataHeader.get(0), pastila1); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), pastila2);
+        listDataChild.put(listDataHeader.get(2), pastila3);
     }
 
 }
